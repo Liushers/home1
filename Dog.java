682a6e69346baded2548ad22a07b0ef288685f87
luba <goddesslove1605@gmail.com>
2020-11-20 16:18:57 +0100

    I am

---
+++ Dog.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.java_one1;
+
+public class Dog extends Animal {
+    public Dog() {
+        super("собака", 500, 10, 0.5);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.java_one1;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog lucky = new Dog();
+        lucky.run(100);
+        lucky.swim(5);
+        lucky.jump(3);
+
+        Cat barsik = new Cat();
+        barsik.swim(10);
+
+    }
+}
